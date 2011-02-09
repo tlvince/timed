ad13cb6c3c63e949fe3aa6470c36fb315e7be242
Tom Vincent <tlvince+linux@gmail.com>
2011-02-09 14:57:55 +0800

    Add existing options

--- timed/application.py
+++ timed/application.py
@@ -11,6 +11,17 @@ def buildArguments():
     parser = argparse.ArgumentParser(description="A command-line time tracker.")
     parser.add_argument("-v", "--version", action="version", version="0.13",
         help="show version information and exit")
+    parser.add_argument("-q", "--quiet", action="store_true",
+        help="print only the current active project")
+
+    subparser = parser.add_subparsers(title="sub-commands")
+    subparser.add_parser("status", help="print current status")
+    start = subparser.add_parser("start", help="start tracking for [project]")
+    start.add_argument("project", help="the project name")
+    subparser.add_parser("stop", help="stop tracking for the current project")
+    subparser.add_parser("summary",
+        help="print a summary of hours for all projects")
+
     return parser
 
 def parseArguments(parser):
