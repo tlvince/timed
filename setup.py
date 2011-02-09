1e922bdf1144a05f7f0877b381253ba36d2a567f
Tom Vincent <tlvince+linux@gmail.com>
2011-02-09 11:20:11 +0800

    Rewrite setup to py3

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
-from setuptools import setup
+#!/usr/bin/env python3
+# Copyright 2011 Tom Vincent <http://www.tlvince.com/contact/>
 
+"""The distutils script to build and install timed."""
+ 
+from distutils.core import setup
+ 
 setup(
-  name='timed',
-  version='0.13',
-  description="command-line time tracking",
-  long_description=open('README').read(),
-  url='http://adeel.github.com/timed',
-  author='Adeel Ahmad Khan',
-  author_email='adeel2@umbc.edu',
-  classifiers=[
-    'Development Status :: 4 - Beta',
-    'Environment :: Console',
-    'License :: OSI Approved :: BSD License',
-    'Natural Language :: English',
-    'Operating System :: POSIX',
-    'Topic :: Utilities',
-  ],
-  py_modules=['timed', 'cmdapp'],
-  entry_points={'console_scripts': ['timed = timed:main']},
-  zip_safe=False,
+    name  =  'timed',
+    version = '0.1.1',
+    packages = ['timed'],
+    scripts = ['bin/timed'],
+    description = "command-line time tracking",
+    long_description = open('README').read(),
+    url = 'https://github.com/tlince/timed/py3',
+    author = 'Tom Vincent',
+    author_email = 'http://www.tlvince.com/contact/',
+    license = 'BSD',
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 4 - Beta',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'Topic :: Office/Business :: Scheduling',
+    ]
 )
+
